6af47048f5a2ae31d5f2e3856ed454da0619a8ac
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-03-26 10:11:02 -0700

    Update scenario.hpp

--- include/scenario.hpp
+++ include/scenario.hpp
@@ -29,3 +29,20 @@ struct ScenarioScore {
     double      delta_R;
     bool        cpvm_safe;   // all nodes safety_ok & viability_ok
 };
+
+struct BasinScore {
+    double K_total;
+    double E_total;
+    double R_total;
+};
+
+BasinScore compute_basin_score(const NexusShard& shard) {
+    BasinScore b{0.0, 0.0, 0.0};
+    for (const auto& n : shard.nodes) {
+        auto res = evaluate_ceim(n);
+        b.K_total += res.Kn;
+        b.E_total += res.E;
+        b.R_total += res.R;
+    }
+    return b;
+}
